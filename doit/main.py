cefedb64f1fa089979876fa32f253ac45bfd2ac3
ghgit1798 <ghgoo1798@gmail.com>
2022-09-20 23:11:01 +0900

    백준 11660_구간 합 구하기2

--- doit/main.py
+++ doit/main.py
@@ -1,19 +1,18 @@
 import sys
 input = sys.stdin.readline
+n, m = map(int, input().split())
+A = [[0]*(n+1)]
+D = [[0]*(n+1) for _ in range(n+1)]
 
-n,m = map(int, input().split())
-table = [[0]*(n+1) for _ in range(n+1)]
-psum = [[0]*(n+1) for _ in range(n+1)]
-
-for i in range(1, n+1):
-    tsum = list(map(int, input().split()))
-    for j in range(1, n+1):
-        table[i][j] = tsum[j-1]
+for i in range(n):
+    A_row = [0] + [int(x) for x in input().split()]
+    A.append(A_row)
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        psum[i][j] = psum[i][j-1]+psum[i-1][j]-psum[i-1][j-1]+table[i][j]
+        D[i][j] = D[i][j-1]+D[i-1][j]-D[i-1][j-1]+A[i][j]
 
 for _ in range(m):
-    x1,y1,x2,y2 = map(int, input().split())
-    print(psum[x2][y2]-psum[x1-1][y1-1])
+    x1, y1, x2, y2 = map(int, input().split())
+    result = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1]
+    print(result)
