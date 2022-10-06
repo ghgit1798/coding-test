cd4b99f029bbbfa2a64f8de6b6b7cd8bb286a5c0
ghgit1798 <ghgoo1798@gmail.com>
2022-10-07 07:56:54 +0900

    17298 오큰수 구하기 풀이 추가

--- doit/17298_오큰수 구하기.py
+++ doit/17298_오큰수 구하기.py
@@ -22,3 +22,24 @@ while stack:
     answer[j] = -1
 
 print(' '.join(map(str, answer)))
+
+
+'''
+n = int(input())
+ans = [0]*n
+A = list(map(int, input().split()))
+myStack = []
+
+for i in range(n):
+    # 스택이 비어 있지 않고 현재 수열의 스택 top 인덱스가 가리키는 수열보다 큰 경우
+    while myStack and A[myStack[-1]] < A[i]:
+        ans[myStack.pop()] = A[i] # 정답 리스트에 오큰수를 현재 수열로 저장하기
+    myStack.append(i)
+
+while myStack:
+    ans[myStack.pop()] = -1
+
+result = ""
+
+print(" ".join(map(str, ans)))
+'''
--- doit/main.py
+++ doit/main.py
@@ -1,24 +1,17 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
-myList = list(map(int, input().split()))
-answer = [0]*n
-stack = []
+ans = [0]*n
+A = list(map(int, input().split()))
+myStack = []
 
 for i in range(n):
-    if not stack:
-        stack.append(i)
-    elif myList[stack[-1]] < myList[i]:
-        while stack and myList[stack[-1]] < myList[i]:
-            j = stack.pop()
-            answer[j] = myList[i]
-        stack.append(i)
-    elif myList[stack[-1]] >= myList[i]:
-        stack.append(i)
+    # 스택이 비어 있지 않고 현재 수열의 스택 top 인덱스가 가리키는 수열보다 큰 경우
+    while myStack and A[myStack[-1]] < A[i]:
+        ans[myStack.pop()] = A[i] # 정답 리스트에 오큰수를 현재 수열로 저장하기
+    myStack.append(i)
 
-while stack:
-    j = stack.pop()
-    answer[j] = -1
+while myStack:
+    ans[myStack.pop()] = -1
 
-print(' '.join(map(str, answer)))
+result = ""
+
+print(" ".join(map(str, ans)))
