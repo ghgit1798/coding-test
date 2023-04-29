f17688def4afc4d7eddd49da82eb11a735e05dab
ghgit1798 <ghgoo1798@gmail.com>
2023-04-29 12:30:25 +0900

    0429 정렬 실전문제풀이

---
+++ 이것이 코딩테스트다/P175 위에서 아래로.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+nums = [int(input()) for _ in range(n)]
+count = [0]*(max(nums)+1)
+
+for num in nums:
+    count[num] += 1
+
+for i in range(len(count)-1, -1, -1):
+    if count[i]:
+        print(i, end=' ')
+
+# 답안 2
+# n = int(input())
+# array = []
+# for i in range(n):
+#     array.append(int(input()))
+#
+# # 파이썬 기본 정렬 라이브러리를 이용하게 정렬 수행
+# array = sorted(array, reverse=True)
+#
+# # 정렬이 수행된 결과를 출력
+# for i in array:
+#     print(i, end = ' ')
---
+++ 이것이 코딩테스트다/P179 두 배열의 원소 교체.py
@@ -0,0 +1,28 @@
+# n, k = map(int, input().split())
+# a = list(map(int, input().split()))
+# b = list(map(int, input().split()))
+#
+# for _ in range(k):
+#     aMin = min(a)
+#     bMax = max(b)
+#     if aMin < bMax:
+#         a.append(bMax)
+#         a.remove(aMin)
+#         b.remove(bMax)
+#
+# print(sum(a))
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.sort()
+b.sort(reverse=True)
+
+for i in range(k):
+    if a[i] < b[i]:
+        a[i], b[i] = b[i], a[i]
+    else:
+        break
+
+print(sum(a))
