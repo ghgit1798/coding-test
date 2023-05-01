ad0f95d033b36366a8d3191dac0347b0e9b2fe1a
ghgit1798 <ghgoo1798@gmail.com>
2023-05-01 16:21:47 +0900

    0501 이진탐색

---
+++ 이것이 코딩테스트다/P186 7-2 재귀 함수로 구현한 이진탐색 소스코드.py
@@ -0,0 +1,20 @@
+# 이진 탐색 소스코드 구현(반복문)
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start+end)//2
+    if target == array[mid]:
+        return mid
+    elif target < array[mid]:
+        return binary_search(array, target, start, mid-1)
+    else:
+        return binary_search(array, target, mid+1, end)
+
+n, target = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n-1)
+if result == None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(result + 1)
