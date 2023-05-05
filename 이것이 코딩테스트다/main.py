37ebae870ecf4d982e8044e80d4c62babf7200af
ghgit1798 <ghgoo1798@gmail.com>
2023-05-05 19:48:24 +0900

    0505 다이나믹 프로그래밍

--- 이것이 코딩테스트다/main.py
+++ 이것이 코딩테스트다/main.py
@@ -1,19 +1,9 @@
-n, m = map(int, input().split())
-array = list(map(int, input().split()))
+n = int(input())
+d = [0]*1001
 
-# 정답인 높이 h는 최소 0에서부터 최대 max(array)까지이다.
-start, end = 0, max(array)
-answer = 0
-while start <= end:
-    mid = (start+end) // 2
-    sum = 0
-    for num in array:
-        if mid < num:
-            sum = sum + (num-mid)
-    if sum >= m:
-        answer = mid
-        start = mid + 1
-    else:
-        end = mid - 1
+d[1] = 1
+d[2] = 3
+for i in range(3, n+1):
+    d[i] = (d[i-1]+2*d[i-2]) % 796796
 
-print(answer)
+print(d[n])
