f49479db36e02435894db2a8c81e1bf1f78e453b
ghgit1798 <ghgoo1798@gmail.com>
2023-05-06 12:38:44 +0900

    0506 다이나믹 프로그래밍

--- 이것이 코딩테스트다/main.py
+++ 이것이 코딩테스트다/main.py
@@ -1,9 +1,17 @@
-n = int(input())
-d = [0]*1001
+n, m = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+dp = [10001]*(m+1)
 
-d[1] = 1
-d[2] = 3
-for i in range(3, n+1):
-    d[i] = (d[i-1]+2*d[i-2]) % 796796
+dp[0] = 0
+# d[m]이 0이면 만들 수 없는 수. d[m]의 최소값을 구하는 것이 목표.
+for i in range(n):
+    for j in range(coins[i], m+1):
+        if dp[j-coins[i]] != 10001: # (i-k)원을 만드는 방법이 존재하는 경우
+            dp[j] = min(dp[j], dp[j-coins[i]]+1)
 
-print(d[n])
+if dp[m] == 10001:
+    print(-1)
+else:
+    print(dp[m])
+
+
