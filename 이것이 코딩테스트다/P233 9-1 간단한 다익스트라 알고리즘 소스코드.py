1ddcdf8fdc46b45e3eb2800810751187e7c2284f
ghgit1798 <ghgoo1798@gmail.com>
2023-05-19 21:36:43 +0900

    0519 다익스트라 알고리즘 - 우선순위 큐

--- 이것이 코딩테스트다/P233 9-1 간단한 다익스트라 알고리즘 소스코드.py
+++ 이것이 코딩테스트다/P233 9-1 간단한 다익스트라 알고리즘 소스코드.py
@@ -41,3 +41,47 @@ for i in range(1, n+1):
         print("INFINITY")
     else:
         print(distance[i])
+
+################################################################
+
+## version 2
+import heapq
+
+def dijkstra(graph, start):
+    # 그래프의 정점 수
+    num_vertices = len(graph)
+
+    # 시작 정점으로부터의 최단 거리를 저장하는 리스트
+    distance = [float('inf')] * num_vertices
+    distance[start] = 0
+
+    # 방문한 정점을 저장하는 집합
+    visited = set()
+
+    # 시작 정점을 우선순위 큐에 삽입
+    queue = [(0, start)]
+
+    while queue:
+        # 우선순위 큐에서 최단 거리가 가장 짧은 정점을 선택
+        dist, current = heapq.heappop(queue)
+
+        # 이미 방문한 정점이라면 건너뜀
+        if current in visited:
+            continue
+
+        # 현재 정점을 방문한 것으로 표시
+        visited.add(current)
+
+        # 현재 정점과 인접한 정점들을 탐색
+        for neighbor, weight in graph[current]:
+            # 시작 정점에서 인접 정점까지의 거리
+            new_distance = distance[current] + weight
+
+            # 현재까지의 최단 거리보다 더 짧은 거리를 발견한 경우, 업데이트
+            if new_distance < distance[neighbor]:
+                distance[neighbor] = new_distance
+                # 우선순위 큐에 새로운 거리와 정점을 삽입
+                heapq.heappush(queue, (new_distance, neighbor))
+
+    return distance
+
