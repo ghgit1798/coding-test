2f85e00c308829462e1c3973d924a327b599ba82
ghgit1798 <ghgoo1798@gmail.com>
2023-06-06 23:23:52 +0900

    0606 서로소 집합 union-find

--- 이것이 코딩테스트다/P269 10-1 기본적인 서로소 집합 알고리즘 소스코드.py
+++ 이것이 코딩테스트다/P269 10-1 기본적인 서로소 집합 알고리즘 소스코드.py
@@ -2,8 +2,8 @@
 def find_parent(parent, x):
     # 루트 노드가 아니라면, 루트 노드를 찾을때까지 재귀적 호출
     if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
+        return find_parent(parent, parent[x])
+    return x
 
 # 두 원소가 속한 집합을 합치기
 def union_parent(parent, a, b):
@@ -22,20 +22,20 @@ parent = [0] * (v+1) # 부모 테이블 초기화
 for i in range(1, v+1):
     parent[i] = i
 
-cycle = False # 사이클 발생 여부
-
+# union 연산을 각각 수행
 for i in range(e):
     a, b = map(int, input().split())
-    # 사이클이 발생한 경우 종료
-    if find_parent(parent, a) == find_parent(parent, b):
-        cycle = True
-        break
-    # 사이클이 발생하지 않았다면 합집함(union) 수행
-    else:
-        union_parent(parent, a, b)
+    union_parent(parent, a, b)
 
-if cycle:
-    print("사이클이 발생했습니다.")
-else:
-    print("사이클이 발생하지 않았습니다.")
+# 각 원소가 속한 집합 출력
+print('각 원소가 속한 집합: ', end='')
+for i in range(1, v+1):
+    print(find_parent(parent, i), end='')
+
+print()
+
+# 부모 테이블 내용 출력
+print('부모 테이블: ', end='')
+for i in range(1, v+1):
+    print(parent[i], end='')
 
