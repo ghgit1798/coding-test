d0b877f5ce389f29e4eab1f640cb85a00e12f66a
ghgit1798 <ghgoo1798@gmail.com>
2023-06-29 21:18:01 +0900

    0629 최단경로 복습

--- 이것이 코딩테스트다/P222 효율적인 화폐구성.py
+++ 이것이 코딩테스트다/P222 효율적인 화폐구성.py
@@ -15,3 +15,21 @@ if d[m] == 10001:
     print(-1)
 else:
     print(d[m])
+
+# 0629 복습 답안
+n, m = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+arr = sorted(arr)
+dp = [99999]*(10001)
+for coin in arr:
+    dp[coin] = 1
+
+for i in range(1, m+1):
+    for coin in arr:
+        if i - coin >= 0 and dp[i-coin] != 99999:
+            dp[i] = min(dp[i], dp[i-coin]+1)
+
+if dp[m] != 99999:
+    print(dp[m])
+else:
+    print(-1)
--- 이것이 코딩테스트다/main.py
+++ 이것이 코딩테스트다/main.py
@@ -1,9 +1,30 @@
-n = int(input())
-dp = [0]*(n+1)
-dp[1] = 1
-dp[2] = 3
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
 
-for i in range(3, n+1):
-    dp[i] = (dp[i-1] + 2*dp[i-2]) % 796796
+n, m = map(int, input().split())
+start = int(input())
+graph = [[] for i in range(n+1)]
+distance = [INF] * (n+1)
 
-print(dp[n])
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b,c))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            # distance[now]는 처리되지 않았다면 INF(무한임). 따라서 처리되지 않았다면 distance[now]가 더 큼
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
