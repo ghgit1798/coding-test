35f7bb73d424260e4f896b4f8fbcfd3969d6d1ee
ghgit1798 <ghgoo1798@gmail.com>
2023-07-07 16:40:56 +0900

    0707 위상정렬 복습

--- 이것이 코딩테스트다/main.py
+++ 이것이 코딩테스트다/main.py
@@ -1,33 +1,36 @@
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
+from collections import deque
+import copy
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
+n = int(input())
 
-v, e = map(int, input().split())
-parent = [0] * (v+1)
+indegree = [0]*(n+1)
+graph = [[] for _ in range(n+1)]
+time = [0]*(n+1)
 
-for i in range(1, v+1):
-    parent[i] = i
+for i in range(1, n+1):
+    data = list(map(int, input().split()))
+    time[i] = data[0]
+    for x in data[1:-1]:
+        indegree[i] += 1
+        graph[x].append(i)
 
-cycle = False
+def topology_sort():
+    result = copy.deepcopy(time)
+    q = deque()
 
-for i in range(e):
-    a, b = map(int, input().split())
-    if find_parent(parent, a) == find_parent(parent, b):
-        cycle = True
-        break
-    else:
-        union_parent(parent, a, b)
+    for i in range(1, n+1):
+        if indegree[i] == 0:
+            q.append(i)
 
-if cycle:
-    print("사이클 발생")
-else:
-    print("사이클 발생 X")
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            result[i] = max(result[i], result[now]+time[i])
+            indegree[i] -= 1
+            if indegree[i] == 0:
+                q.append(i)
+
+    for i in range(1, n+1):
+        print(result[i])
+
+topology_sort()
