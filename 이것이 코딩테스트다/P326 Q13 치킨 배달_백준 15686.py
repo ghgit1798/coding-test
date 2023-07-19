48759d50a28d6dc02a7e700879b988d947f21ba9
ghgit1798 <ghgoo1798@gmail.com>
2023-07-19 20:06:31 +0900

    0719 구현 문제 풀이_치킨배달

--- 이것이 코딩테스트다/P326 Q13 치킨 배달_백준 15686.py
+++ 이것이 코딩테스트다/P326 Q13 치킨 배달_백준 15686.py
@@ -6,22 +6,26 @@ chicken, house = [], []
 
 for r in range(n):
     for c in range(n):
-        if graph[r][c] == 1:
+        if graph[r][c] == 1:        # 일반 집
             house.append((r,c))
-        elif graph[r][c] == 2:
+        elif graph[r][c] == 2:      # 치킨집
             chicken.append((r,c))
 
+# 모든 치킨집 중에서 m개의 치킨집을 뽑는 조합 계산
+candidates = list(combinations(chicken, m))
+
+# 치킨 거리의 합을 계산하는 함수
 def get_sum(candidate):
     result = 0
     for hx, hy in house:
         dist = 1e9
         for cx, cy in candidate:
             dist = min(dist, abs(hx-cx)+abs(hy-cy))
+        # 가장 가까운 치킨집까지의 거리를 더하기
         result = result + dist
     return result
 
-candidates = list(combinations(chicken, m))
-
+# 치킨 거리의 합의 최소를 찾아 출력
 result = 1e9
 for candidate in candidates:
     result = min(result, get_sum(candidate))
