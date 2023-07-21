e36da087faed2189bb597b2072badad6a43338a6
ghgit1798 <ghgoo1798@gmail.com>
2023-07-21 19:53:25 +0900

    0721 구현문제 복습

--- 이것이 코딩테스트다/main.py
+++ 이것이 코딩테스트다/main.py
@@ -1,22 +1,25 @@
 from itertools import permutations
 
 def solution(n, weak, dist):
-    # 길이를 2배로 늘려서 '원형'을 일자 형태로 변형
     length = len(weak)
     for i in range(length):
         weak.append(weak[i]+n)
+    candidates = list(permutations(dist, len(dist)))
+
     answer = len(dist) + 1
+    # 첫번째부터 length-1까지 시작점 테스트
     for start in range(length):
-        for friends in list(permutations(dist, len(dist))):
-            count = 1 # 투입할 친구의 수
-            position = weak[start] + friends[count-1]
+        for candidate in candidates:
+            count = 1
+            position = weak[start] + candidate[count-1]
             for i in range(start, start+length):
                 if position < weak[i]:
                     count += 1
                     if count > len(dist):
                         break
-                    position = weak[i] + friends[count-1]
+                    position = weak[i] + candidate[count-1]
             answer = min(answer, count)
+
     if answer > len(dist):
         return -1
     return answer
