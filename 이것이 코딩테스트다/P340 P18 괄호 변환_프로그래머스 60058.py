bfe58e72fec35aaa0a436a24715ac6623b742fe8
ghgit1798 <ghgoo1798@gmail.com>
2023-07-27 21:08:31 +0900

    0727 구현 올바른 문자열 문제풀이

--- 이것이 코딩테스트다/P340 P18 괄호 변환_프로그래머스 60058.py
+++ 이것이 코딩테스트다/P340 P18 괄호 변환_프로그래머스 60058.py
@@ -8,7 +8,6 @@ def balanced_index(p):
         if count == 0:
             return i
 
-
 def check_proper(p):
     count = 0
     for i in p:
@@ -20,21 +19,22 @@ def check_proper(p):
             count -= 1
     return True
 
-
 def solution(p):
     answer = ''
     if p == '':
         return answer
     index = balanced_index(p)
-    u = p[:index + 1]
-    v = p[index + 1:]
+    u = p[:index+1]
+    v = p[index+1:]
+    # 올바른 괄호 문자열이 아니라면 아래의 과정을 수행
     if check_proper(u):
         answer = u + solution(v)
+    # 올바른 괄호 문자열이 아니라면 아래의 과정 수행
     else:
         answer = '('
         answer += solution(v)
         answer += ')'
-        u = list(u[1:-1])  # 첫 번째와 마지막 문자를 제거
+        u = list(u[1:-1])
         for i in range(len(u)):
             if u[i] == '(':
                 u[i] = ')'
@@ -42,3 +42,4 @@ def solution(p):
                 u[i] = '('
         answer += "".join(u)
     return answer
+
