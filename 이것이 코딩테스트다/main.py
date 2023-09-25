0476387c2acb09ff9ba9cec3c77ad95dd85c411d
ghgit1798 <ghgoo1798@gmail.com>
2023-09-25 22:55:59 +0900

    0925 dfs, bfs

--- 이것이 코딩테스트다/main.py
+++ 이것이 코딩테스트다/main.py
@@ -2,26 +2,24 @@ from collections import deque
 
 n, m = map(int, input().split())
 graph = [list(map(int, input())) for _ in range(n)]
+visited = [[0]*m for _ in range(n)]
 dx, dy = [-1,1,0,0], [0,0,-1,1]
-answer = 0
 
 def bfs(graph, x, y):
     q = deque()
     q.append([x, y])
     while q:
         x, y = q.popleft()
-        graph[x][y] = 2
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+            if nx<0 or ny<0 or nx>=n or ny>=m:
                 continue
-            if graph[nx][ny] == 0:
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
                 q.append([nx, ny])
 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 0:
-            bfs(graph, i, j)
-            answer += 1
 
-print(answer)
+graph[0][0] = 1
+bfs(graph, 0, 0)
+
+print(graph[n-1][m-1])
